7d879b03c99478d186cd9eef7c5754c3c353fba6
Túlio Hoffimann <tuliohmendes@gmail.com>
2014-06-18 19:01:17 -0300

    Starting to give life to Today Lessons Card :-)

--- app/src/main/java/com/thm/unicap/app/dashboard/TodayLessonsCard.java
+++ app/src/main/java/com/thm/unicap/app/dashboard/TodayLessonsCard.java
@@ -1,21 +1,18 @@
 package com.thm.unicap.app.dashboard;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.DecelerateInterpolator;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.echo.holographlibrary.PieGraph;
-import com.echo.holographlibrary.PieSlice;
-import com.echo.holographlibrary.Utils;
 import com.thm.unicap.app.R;
 import com.thm.unicap.app.UnicapApplication;
 import com.thm.unicap.app.model.Student;
 import com.thm.unicap.app.model.Subject;
-import com.thm.unicap.app.model.SubjectStatus;
 
 import java.util.List;
 
@@ -30,9 +27,35 @@ public class TodayLessonsCard extends Card {
     @Override
     public void setupInnerViewElements(ViewGroup parent, View view) {
 
-//        Student student = UnicapApplication.getStudent();
-//
-//        List<Subject> subjects = student.getActualSubjects();
+        Student student = UnicapApplication.getStudent();
+
+        List<Subject> subjects = student.getActualSubjects();
+        subjects = subjects.subList(0,2);
+
+        ViewGroup lessonsContainer = (ViewGroup) parent.findViewById(R.id.card_today_lessons_container);
+        LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        Animation expand = AnimationUtils.loadAnimation(mContext, R.anim.expand_from_left_to_right);
+
+        for (Subject subject : subjects) {
+            View lessonsEntry = layoutInflater.inflate(R.layout.card_today_lessons_entry, lessonsContainer, false);
+
+            TextView subject_name_abbreviation = (TextView) lessonsEntry.findViewById(R.id.subject_name_abbreviation);
+            TextView subject_name = (TextView) lessonsEntry.findViewById(R.id.subject_name);
+            TextView subject_room = (TextView) lessonsEntry.findViewById(R.id.subject_room);
+            TextView subject_schedule = (TextView) lessonsEntry.findViewById(R.id.subject_schedule);
+            View subject_progress = lessonsEntry.findViewById(R.id.subject_progress);
+
+            subject_name_abbreviation.setBackgroundResource(subject.getColorResource());
+            subject_name_abbreviation.setText(subject.getNameAbbreviation());
+            subject_name.setText(subject.name);
+            subject_room.setText(subject.getActualSubjectStatus().room);
+            subject_schedule.setText(subject.getActualSubjectStatus().schedule);
+            subject_progress.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 30f));
+
+            lessonsContainer.addView(lessonsEntry);
+
+            subject_progress.startAnimation(expand);
+        }
 
     }
 
