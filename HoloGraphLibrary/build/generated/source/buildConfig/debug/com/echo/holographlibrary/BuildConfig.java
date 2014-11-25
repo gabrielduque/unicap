a51a5e6ad0b28500206c6297b23d01604045811f
Túlio Hoffimann <tuliohmendes@gmail.com>
2014-11-25 15:08:48 -0300

    Updating sdk build tools version.

--- HoloGraphLibrary/build/generated/source/buildConfig/debug/com/echo/holographlibrary/BuildConfig.java
+++ HoloGraphLibrary/build/generated/source/buildConfig/debug/com/echo/holographlibrary/BuildConfig.java
@@ -5,7 +5,7 @@ package com.echo.holographlibrary;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.echo.holographlibrary";
+  public static final String APPLICATION_ID = "com.echo.holographlibrary";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
--- HoloGraphLibrary/build/generated/source/buildConfig/release/com/echo/holographlibrary/BuildConfig.java
+++ HoloGraphLibrary/build/generated/source/buildConfig/release/com/echo/holographlibrary/BuildConfig.java
@@ -5,7 +5,7 @@ package com.echo.holographlibrary;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String PACKAGE_NAME = "com.echo.holographlibrary";
+  public static final String APPLICATION_ID = "com.echo.holographlibrary";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
